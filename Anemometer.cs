fb995d41208cf71a663f9dc2104429127b577268
NANDbrew <zortman@gmail.com>
2026-07-19 16:44:28 -0500

    add code for barometer and anemometer

---
+++ Anemometer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace WindiBridge
+{
+    [RequireComponent(typeof(Collider))]
+    public class Anemometer : Weathervane
+    {
+        public Transform needle;
+        public Transform spinner;
+        public float spinnerMult = 50f;
+        public float needleMult = 7.2f;
+        public float responseMult = 0.5f;
+        public float value = 0f;
+        float lastValue = 0f;
+        float startAngle = 0;
+        bool inInventory;
+        public CollisionTracker colTracker;
+        public ShipItem shipItem;
+
+        public void Start()
+        {
+            startAngle = needle.localEulerAngles.z;
+            if (colTracker != null) StartCoroutine(InitColTracker());
+        }
+
+        public void OnEnterInventory()
+        {
+            inInventory = true;
+        }
+        public void OnLeaveInventory()
+        {
+            inInventory = false;
+        }
+
+        private IEnumerator InitColTracker()
+        {
+            yield return new WaitUntil(() => shipItem.itemRigidbodyC != null);
+            colTracker.transform.SetParent(shipItem.itemRigidbodyC.transform, false);
+        }
+
+        public override void LateUpdate()
+        {
+            if (GameState.wasInSettingsMenu || !GameState.playing || GameState.sleeping) return;
+            if (!shipItem.held && colTracker && colTracker.collisions > 0)
+            {
+                value = 0;
+                lastValue = Mathf.Lerp(lastValue, value, Time.deltaTime * (responseMult * 5));
+                var tangle = startAngle + lastValue * needleMult;
+                needle.localEulerAngles = new Vector3(0f, 0f, tangle);
+                return;
+            }
+            if (shadowedBy)
+            {
+                value = worldVelocity.magnitude;
+            }
+            else if (!inInventory)
+            {
+                value = (Wind.currentWind - worldVelocity).magnitude;
+            }
+            else value = 0;
+
+            lastValue = Mathf.Lerp(lastValue, value, Time.deltaTime * responseMult);
+            var angle = startAngle + lastValue * needleMult;
+            needle.localEulerAngles = new Vector3(0f, 0f, angle);
+
+            spinner.Rotate(0f, Time.deltaTime * lastValue * spinnerMult, 0f);
+
+            shipItem.description = Math.Round(value, 1).ToString() + "\ntarget angle: " + Mathf.Round(angle).ToString();
+        }
+
+    }
+
+}
---
+++ Barometer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace WindiBridge
+{
+    internal class Barometer : MonoBehaviour
+    {
+        ShipItem shipItem;
+        HangableItem hangable;
+        public Transform needle;
+        public float minAngle = 10f;
+        public float maxAngle = 170f;
+        static float maxDist = 34000f;
+        public float angleStep = 0.2f;
+        float errorTime = 0f;
+        float currentError = 0f;
+        float rangeMult = 1f;
+
+        public void Start()
+        {
+            shipItem = GetComponent<ShipItem>();
+            hangable = GetComponent<HangableItem>();
+
+            UpdateAngle(true);
+        }
+
+        private void Update()
+        {
+            //Debug.Log("angle step = " + angleStep);
+            maxDist = RegionBlender.instance.blendedRegion.stormRange * rangeMult;
+            UpdateAngle(false);
+            if (errorTime <= 0f)
+            {
+                currentError = GetError();
+                errorTime = UnityEngine.Random.Range(1, 5);
+            }
+            errorTime -= Time.deltaTime;
+        }
+
+        private void UpdateAngle(bool instant)
+        {
+            float normalizedDist = Mathf.InverseLerp(maxDist, 0f, WeatherStorms.currentStormDistance);
+            float angle = Mathf.Lerp(minAngle, maxAngle, Mathf.Clamp01(normalizedDist + currentError));
+            float lerpedAngle = Mathf.Lerp(needle.localEulerAngles.z, angle, Time.deltaTime * angleStep);
+
+            //float derp = instant ? angle : Mathf.MoveTowardsAngle(needle.localEulerAngles.z, angle, angleStep);
+
+            needle.localEulerAngles = new Vector3(0f, 0f, lerpedAngle);
+            
+            shipItem.description = Math.Round(normalizedDist, 3).ToString() + "\n" + Math.Round(Mathf.Lerp(31f, 28f, normalizedDist), 2).ToString();
+
+        }
+
+        private float GetError()
+        {
+            if (hangable != null)
+            {
+                if (!hangable.IsHanging())
+                {
+                    //TODO: if not hanging, add random inaccuracies by verticality
+                    var tilt = Vector3.Angle(transform.up.normalized, Vector3.up);
+                    if (tilt > 3f)
+                    {
+                        float error = tilt / 360;
+                        return UnityEngine.Random.Range(-error, error);
+                    }
+                }
+            }
+            return 0f;
+        }
+
+    }
+}
