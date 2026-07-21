b2b5c41bb3bbdb9dbd8989917617532392131cb0
NANDbrew <zortman@gmail.com>
2026-07-21 00:36:38 -0500

    Added separate RolloverTumbler class for potential use with chiplogs

---
+++ RolloverTumbler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace WindiBridge
+{
+    internal class RolloverTumbler : MonoBehaviour
+    {
+        public int sideCount;
+        public Transform sourceNeedle;
+
+        public void Update()
+        {
+            float num = 360 / sideCount;
+            float newFace = Mathf.Min(num * Mathf.Floor(Mathf.Abs(sourceNeedle.localEulerAngles.z / 360)), 360 - num);
+            float speedMult = num * 2 / Mathf.Abs(transform.localEulerAngles.z - newFace);
+            transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, Mathf.Lerp(transform.localEulerAngles.z, newFace, Time.deltaTime * Mathf.Pow(speedMult, 1.8f)));
+            
+        }
+    }
+}
